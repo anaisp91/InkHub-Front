90a5561b0f389ae60582aacc289e7eb9c2743090
Anais <anais.planas.91@gmail.com>
2026-07-09 13:54:33 +0200

    :modificacion css landing page

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import { Nav } from "../Nav/Nav";
 export const Header = () => {
   return (
-    <header className="sticky top-0 left-0 w-full z-50 bg-white border-b backdrop-blur-lg bg-opacity-80">
+    <header className="fixed top-0 left-0 w-full z-50 bg-black border-b backdrop-blur-lg bg-opacity-80">
       <div className="mx-auto max-w-7xl px-6 sm:px-6 lg:px-8 ">
         <div className="relative flex h-16 justify-between">
           <div className="flex flex-1 items-stretch justify-start">
             <img
-              className="block h-auto w-auto"
-              src="/images/logos/logo-inkhub.png"
+              className="block  min-w-5"
+              src="/images/logos/InkHub-logo-blanco.png"
             />
           </div>
           <div className="flex-shrink-0 flex px-2 py-3 items-center space-x-8">
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,20 +1,31 @@
 export const Home = () => {
   return (
-    <div className="min-h-screen min-w-full flex-col items-center justify-start text-center p-28 bg-slate-100">
-      <div>
+    <div className="relative h-screen overflow-hidden bg-[url('/images/logos/foto-fondo.jpg')] bg-cover bg-center bg-no-repeat h-64 md:h-96 lg:h-screen">
+      <div className="absolute inset-0 bg-black/80"></div>
+      <div className="relative z-10">
         <img
           alt="InkHub Logo"
-          src="/images/logos/Inkhub.png"
-          className="mx-auto h-auto w-[18rem] m-4"
+          src="/images/logos/InkHub-logo2-blanco.png"
+          className="mx-auto h-auto w-[24rem] mt-20 mb-5 md:mt-32 lg:mt-40 "
         />
-        <div className="bg-slate-300 mt-9 p-7 ">
-          <p className="font-bold font-sans text-[1.5rem] leading-6">
-            GESTIONA TU ESTUDIO DE FORMA DIGITAL
+        <div className="bg-slate-300/20 m-4 p-5 ">
+          <p className="font-medium font-sans text-white text-[1rem] leading-6 text-center">
+            La plataforma para gestionar tu estudio de tatuajes de manera
+            sencilla
           </p>
-          <p className="font-sans text-sm mt-9 ">
-            Centraliza artistas, clientes y consentimientos en una sola
-            plataforma. Olvídate del papel y administra tu estudio de manera más
-            rápìda, segura y profesional.
+          <p className="font-semibold text-white mt-9 text-center">
+            Menos gestion más tiempo para crear
+          </p>
+          <p>
+            Organiza artistas, clientes, consentimientos y toda la información
+            de tu estudio desde un mismo lugar
+          </p>
+          <p>¿Que es InkHub?</p>
+          <p>
+            InkHub nace con el objetico de simplifiacar la gestión diaria de un
+            estudio de tatuajes. Desde una única plataforma el estudio puede
+            administrar a sus artistas, organizar la información de sus clientes
+            y guardar los consetimientos de una forma segura.{" "}
           </p>
         </div>
       </div>
